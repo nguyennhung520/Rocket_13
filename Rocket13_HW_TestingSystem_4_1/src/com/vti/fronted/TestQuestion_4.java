fb13da746cff807f2608737d666f81ebc3ba5788
nguyennhung520 <57894053+nguyennhung520@users.noreply.github.com>
2021-05-28 17:32:23 +0700

    Add files via upload

---
+++ Rocket13_HW_TestingSystem_4_1/src/com/vti/fronted/TestQuestion_4.java
@@ -0,0 +1,13 @@
+package com.vti.fronted;
+
+import com.vti.backend.Question4;
+
+public class TestQuestion_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Question4 ques4 = new Question4();
+		ques4.qLDocument();
+	}
+
+}
